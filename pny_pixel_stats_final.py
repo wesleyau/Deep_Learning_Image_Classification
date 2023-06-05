b673423b779746692f2349418449df1df7a6d5de
wesley au <aucwesley@gmail.com>
2023-06-05 10:04:52 -0500

    1

--- pny_pixel_stats_final.py
+++ pny_pixel_stats_final.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 
-def collect_statistics(folder_path, min_intensity_threshold=0.001):
+def collect_statistics(folder_path):
     file_list = os.listdir(folder_path)
     folder_stats = []
 
@@ -13,12 +13,12 @@ def collect_statistics(folder_path, min_intensity_threshold=0.001):
         elif file_name.endswith('.npy'):
             data = np.load(file_path)
 
+            # Flatten the data array
+            data = data.flatten()
+
             # Skip 0, 1, NaN, and values close to zero
             data = data[(data != 0) & (data != 1) & (~np.isnan(data)) & (np.abs(data) > 1e-6)]
 
-            # Flatten the array
-            data = data.flatten()
-            
             if data.size > 0:
                 # Calculate summary statistics and quantile ranges for pixel intensities
                 stats = {
@@ -42,11 +42,30 @@ def collect_statistics(folder_path, min_intensity_threshold=0.001):
 
     return folder_stats
 
-def save_statistics_to_file(folder_path, folder_stats, output_folder):
+def save_statistics_to_file(folder_path, folder_stats, output_folder, total_files, overall_stats):
     folder_name = os.path.basename(folder_path)
     output_file = os.path.join(output_folder, folder_name + '_stats.txt')
     with open(output_file, 'w') as file:
         file.write('Folder Statistics: ' + folder_name + '\n')
+        file.write('Total Files: ' + str(total_files) + '\n')
+        file.write('\n')
+
+        # Write overall statistics for the folder
+        file.write('Overall Statistics\n')
+        file.write('Min: ' + str(overall_stats['Min']) + '\n')
+        file.write('Max: ' + str(overall_stats['Max']) + '\n')
+        file.write('Mean: ' + str(overall_stats['Mean']) + '\n')
+        file.write('Median: ' + str(overall_stats['Median']) + '\n')
+        file.write('Std: ' + str(overall_stats['Std']) + '\n')
+        file.write('Quantile 1: ' + str(overall_stats['Quantile_1']) + '\n')
+        file.write('Quantile 5: ' + str(overall_stats['Quantile_5']) + '\n')
+        file.write('Quantile 10: ' + str(overall_stats['Quantile_10']) + '\n')
+        file.write('Quantile 25: ' + str(overall_stats['Quantile_25']) + '\n')
+        file.write('Quantile 50: ' + str(overall_stats['Quantile_50']) + '\n')
+        file.write('Quantile 75: ' + str(overall_stats['Quantile_75']) + '\n')
+        file.write('Quantile 90: ' + str(overall_stats['Quantile_90']) + '\n')
+        file.write('Quantile 95: ' + str(overall_stats['Quantile_95']) + '\n')
+        file.write('Quantile 99: ' + str(overall_stats['Quantile_99']) + '\n')
         file.write('\n')
 
         # Write statistics for each file
@@ -86,15 +105,53 @@ os.makedirs(ty_output_folder, exist_ok=True)
 # Traverse the TX machine directory and collect statistics
 for root, dirs, files in os.walk(tx_directory):
     folder_stats = collect_statistics(root)
+    total_files = len(files)
 
     if folder_stats:
+        # Calculate overall statistics for the folder
+        overall_stats = {
+            'Min': np.min([stats['Min'] for stats in folder_stats]),
+            'Max': np.max([stats['Max'] for stats in folder_stats]),
+            'Mean': np.mean([stats['Mean'] for stats in folder_stats]),
+            'Median': np.median([stats['Median'] for stats in folder_stats]),
+            'Std': np.mean([stats['Std'] for stats in folder_stats]),
+            'Quantile_1': np.percentile([stats['Quantile_1'] for stats in folder_stats], 1),
+            'Quantile_5': np.percentile([stats['Quantile_5'] for stats in folder_stats], 5),
+            'Quantile_10': np.percentile([stats['Quantile_10'] for stats in folder_stats], 10),
+            'Quantile_25': np.percentile([stats['Quantile_25'] for stats in folder_stats], 25),
+            'Quantile_50': np.percentile([stats['Quantile_50'] for stats in folder_stats], 50),
+            'Quantile_75': np.percentile([stats['Quantile_75'] for stats in folder_stats], 75),
+            'Quantile_90': np.percentile([stats['Quantile_90'] for stats in folder_stats], 90),
+            'Quantile_95': np.percentile([stats['Quantile_95'] for stats in folder_stats], 95),
+            'Quantile_99': np.percentile([stats['Quantile_99'] for stats in folder_stats], 99)
+        }
+
         # Save statistics to a text file in the TX output folder
-        save_statistics_to_file(root, folder_stats, tx_output_folder)
+        save_statistics_to_file(root, folder_stats, tx_output_folder, total_files, overall_stats)
 
 # Traverse the TY machine directory and collect statistics
 for root, dirs, files in os.walk(ty_directory):
     folder_stats = collect_statistics(root)
+    total_files = len(files)
 
     if folder_stats:
+        # Calculate overall statistics for the folder
+        overall_stats = {
+            'Min': np.min([stats['Min'] for stats in folder_stats]),
+            'Max': np.max([stats['Max'] for stats in folder_stats]),
+            'Mean': np.mean([stats['Mean'] for stats in folder_stats]),
+            'Median': np.median([stats['Median'] for stats in folder_stats]),
+            'Std': np.mean([stats['Std'] for stats in folder_stats]),
+            'Quantile_1': np.percentile([stats['Quantile_1'] for stats in folder_stats], 1),
+            'Quantile_5': np.percentile([stats['Quantile_5'] for stats in folder_stats], 5),
+            'Quantile_10': np.percentile([stats['Quantile_10'] for stats in folder_stats], 10),
+            'Quantile_25': np.percentile([stats['Quantile_25'] for stats in folder_stats], 25),
+            'Quantile_50': np.percentile([stats['Quantile_50'] for stats in folder_stats], 50),
+            'Quantile_75': np.percentile([stats['Quantile_75'] for stats in folder_stats], 75),
+            'Quantile_90': np.percentile([stats['Quantile_90'] for stats in folder_stats], 90),
+            'Quantile_95': np.percentile([stats['Quantile_95'] for stats in folder_stats], 95),
+            'Quantile_99': np.percentile([stats['Quantile_99'] for stats in folder_stats], 99)
+        }
+
         # Save statistics to a text file in the TY output folder
-        save_statistics_to_file(root, folder_stats, ty_output_folder)
+        save_statistics_to_file(root, folder_stats, ty_output_folder, total_files, overall_stats)
