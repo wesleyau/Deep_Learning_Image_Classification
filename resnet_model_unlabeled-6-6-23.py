e80c190d0867625bd96d248269cbb8be336f7cc3
wesley au <aucwesley@gmail.com>
2023-06-07 10:39:42 -0500

    1

--- resnet_model_unlabeled-6-6-23.py
+++ resnet_model_unlabeled-6-6-23.py
@@ -3,12 +3,17 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.models as models
+from torchvision.models import resnet50
+from torchvision.models.resnet import ResNet50_Weights
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 import os
 
 # Define the path to the main directory containing the pass, fail, and unlabeled folders
 main_directory = '/data/wesley/data2/dataset_tx'
+
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define the transform to be applied to the input images
 image_transform = transforms.Compose([
@@ -28,25 +33,26 @@ class CrystalDataset(Dataset):
         for label in ['pass', 'fail', 'unlabeled']:
             label_directory = os.path.join(directory, label)
             if os.path.exists(label_directory):
-                # Iterate over the image files in the pass, fail, or unlabeled directory
-                for file_name in os.listdir(label_directory):
-                    file_path = os.path.join(label_directory, file_name)
-                    self.file_list.append((file_path, label))
+                # Iterate over the crystal directories in the pass, fail, or unlabeled directory
+                for crystal_dir in os.listdir(label_directory):
+                    crystal_dir_path = os.path.join(label_directory, crystal_dir)
+                    if os.path.isdir(crystal_dir_path):
+                        self.file_list.append((crystal_dir_path, label))
         
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, index):
-        file_path, label = self.file_list[index]
+        crystal_dir_path, label = self.file_list[index]
+        file_name = os.listdir(crystal_dir_path)[0]  # select the first image
+        file_path = os.path.join(crystal_dir_path, file_name)
         image = Image.open(file_path).convert('RGB')
-        
         if self.transform is not None:
             image = self.transform(image)
-        
         return image, label
 
 # Create the ResNet model
-model = models.resnet50(pretrained=True)
+model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
 num_features = model.fc.in_features
 model.fc = nn.Linear(num_features, 3)  # Three output classes: pass, fail, and unlabeled
 
