0e7249f6b4bc004607aabb7cabe75f20494683ca
wesley au <aucwesley@gmail.com>
2023-07-18 10:04:55 -0500

    1

--- resnet_2channel_TY_model_confidence_consistent_7-17-23.py
+++ resnet_2channel_TY_model_confidence_consistent_7-17-23.py
@@ -30,24 +30,33 @@ class CustomImageFolder(Dataset):
         self.imgs = []
         self.labels = []
         self.furnace_nums = []
-        
-        classes = os.listdir(root_dir)
+        self.crystal_count = {} 
+        self.crystal_ids = []  
+
+        classes = ['Pass', 'Fail']  # assuming these are the only two classes
         for i, class_ in enumerate(classes):
             class_dir = os.path.join(root_dir, class_)
-            furnaces = os.listdir(class_dir)
-            for furnace in furnaces:
-                furnace_num = int(furnace.split("_")[0][0])  # extract first character as furnace number
-                furnace_dir = os.path.join(class_dir, furnace)
+            #print(f'Class directory: {class_dir}')  
+            crystals = [entry.name for entry in os.scandir(class_dir) if not entry.is_symlink()]
+            #print(f'Found crystals: {crystals}')  # And this line
+            for crystal in crystals:
+                if crystal not in self.crystal_count: 
+                    self.crystal_count[crystal] = 0
+                self.crystal_count[crystal] += 1
+                #print(f'Processing crystal folder: {crystal} - Status: {class_}')
+                furnace_num = int(crystal[0])  # extract first character as furnace number
+                crystal_dir = os.path.join(class_dir, crystal)
                 imgs = []
-                for img_name in sorted(os.listdir(furnace_dir)):
+                for img_name in sorted(os.listdir(crystal_dir)):
                     if img_name.endswith('.png'):
-                        img_path = os.path.join(furnace_dir, img_name)
+                        img_path = os.path.join(crystal_dir, img_name)
                         imgs.append(img_path)
                 # Group every two images together
                 for j in range(0, len(imgs), 2):
                     self.imgs.append([imgs[j], imgs[j + 1]])
                     self.labels.append(i)  # assign label based on class
                     self.furnace_nums.append(furnace_num)
+                    self.crystal_ids.append(crystal)  # store crystal ID
     
     def __len__(self):
         return len(self.imgs)
@@ -58,11 +67,12 @@ class CustomImageFolder(Dataset):
         img = torch.cat([img1, img2], dim=0)  # concatenate along the channel dimension
         label = self.labels[index]
         furnace_num = torch.tensor([self.furnace_nums[index]], dtype=torch.long)  # pass as tensor
-        return img, furnace_num, label
+        crystal_id = self.crystal_ids[index]  # get the crystal ID
+        return img, furnace_num, label, crystal_id  # return the crystal ID along with the other data
 
 # Load the data
-train_data = CustomImageFolder('/data/wesley/2_data/train_test/TX/train', transform=transform)
-val_data = CustomImageFolder('/data/wesley/2_data/train_test/TX/val', transform=transform)
+train_data = CustomImageFolder('/data/wesley/2_data/train_test/TY/train', transform=transform)
+val_data = CustomImageFolder('/data/wesley/2_data/train_test/TY/val', transform=transform)
 
 # Data loaders
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
@@ -90,8 +100,6 @@ class CustomModel(nn.Module):
 
     def forward(self, x, furnace_num):
         x = self.base_model(x)
-        #print(x.shape)  # Add this line
-        #print("Size after base model: ", x.size())  # Add this line
         x = torch.flatten(x, start_dim=1)
         x = torch.cat([x, furnace_num], dim=1)
         x = self.fc(x)
@@ -102,7 +110,7 @@ model = CustomModel(model)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define the loss function and the optimizer
-# For binary classification:
+# For binary classification
 weights = [0.87, 0.13]  # class 0 is "Passes" and class 1 is "Fails"
 class_weights = torch.FloatTensor(weights).to(device)
 criterion = nn.CrossEntropyLoss(weight=class_weights)
@@ -114,7 +122,7 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 # Define the path to the output directory
 optimizer_name = "Adagrad"
 weights_str = '-'.join(str(w) for w in weights)  # convert weights to a string
-output_dir = f'/data/wesley/2_data/model_outputs/TX/ex713{optimizer_name}_{weights_str}_{timestamp}'
+output_dir = f'/data/wesley/2_data/final_model_outputs/TY/consistent{optimizer_name}_{weights_str}_{timestamp}'
 
 # Create the output directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
@@ -123,8 +131,6 @@ if torch.cuda.device_count() > 1:
     print("Let's use", torch.cuda.device_count(), "GPUs!")
     model = nn.DataParallel(model)
     
-
-# Now move the model to the device
 model = model.to(device)
 
 # Lists for saving epoch-wise losses and accuracies
@@ -132,13 +138,15 @@ train_losses, val_losses, train_accs, val_accs = [], [], [], []
 
 best_acc = 0.0  # Track best validation accuracy
 best_epoch = 0 # Track best epoch
+val_acc = 0.0 
+
 
 # Training loop
 for epoch in range(10):  # 10 epochs, adjust as needed
     model.train()  
     train_loss = 0.0
     train_corrects = 0
-    for inputs, furnace_nums, labels in train_loader:
+    for inputs, furnace_nums, labels, crystal_ids in train_loader:
         inputs, furnace_nums, labels = inputs.to(device), furnace_nums.float().to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(inputs, furnace_nums)
@@ -156,8 +164,16 @@ for epoch in range(10):  # 10 epochs, adjust as needed
     val_labels = None
     val_loss = 0.0
     val_corrects = 0
+
+    # Initialize empty lists for prediction probabilities
+    pass_probs = []
+    fail_probs = []
+    
+    # Initialize empty list to store predictions and their associated data
+    epoch_preds = []
+    
     with torch.no_grad():
-        for inputs, furnace_nums, labels in val_loader:
+        for i, (inputs, furnace_nums, labels, crystal_ids) in enumerate(val_loader):
             inputs, furnace_nums, labels = inputs.to(device), furnace_nums.float().to(device), labels.to(device)
             outputs = model(inputs, furnace_nums)
             _, preds = torch.max(outputs, 1)
@@ -172,7 +188,40 @@ for epoch in range(10):  # 10 epochs, adjust as needed
                 val_scores = np.vstack([val_scores, scores])
                 val_labels = np.hstack([val_labels, labels.cpu().numpy()])
 
+            # Iterate over the scores and labels, appending the probabilities to the right list
+            for score, label in zip(scores, labels.cpu().numpy()):
+                if label == 0:  # class 'Pass'
+                    pass_probs.append(score[0])
+                else:  # class 'Fail'
+                    fail_probs.append(score[1])
+            
+            # Code to save probabilities for each crystal in the epoch
+            for j, score in enumerate(scores):
+                crystal_id = crystal_ids[j]  # get the crystal ID
+                human_label = 'Pass' if labels[j] == 0 else 'Fail' # get the crystal label
+                model_label = 'Pass' if preds[j] == 0 else 'Fail'
+                model_probability = score[1]  # score for class 1 (Fail)
+                if model_probability > 0.80: # change thresholds as necessary
+                    confidence = 'Confident Fail'
+                elif model_probability < 0.20: # change threshold as necessary
+                    confidence = 'Confident Pass'
+                else:
+                    confidence = 'Not confident'
+                epoch_preds.append((crystal_id, human_label, model_label, model_probability, confidence))
 
+    # Checking whether the dataset is loaded correctly - If this prints, something is wrong
+    for crystal, count in val_data.crystal_count.items():
+        if count != 1:
+            print(f"Crystal {crystal} appears {count} times in the validation data.")
+
+    with open(os.path.join(output_dir, f'epoch_{epoch + 1}_log.txt'), 'a') as f:
+        for crystal_id, human_label, model_label, model_probability, confidence in epoch_preds:
+            f.write(f'Crystal ID: {crystal_id}\n')
+            f.write(f'Human label: {human_label}\n')
+            f.write(f'Model label: {model_label}\n')
+            f.write(f'Model Probability: {model_probability:.3f}\n')
+            f.write(f'Confidence: {confidence}\n\n')
+            
     # Calculate average losses and accuracies
     train_loss = train_loss / len(train_data)
     val_loss = val_loss / len(val_data)
@@ -189,6 +238,25 @@ for epoch in range(10):  # 10 epochs, adjust as needed
     print(f'Train Loss: {train_loss:.4f} Acc: {train_acc:.4f}')
     print(f'Val Loss: {val_loss:.4f} Acc: {val_acc:.4f}')
 
+    plt.figure(figsize=(12, 6))
+    plt.hist(pass_probs, bins=20, alpha=0.5, color='#009999',)
+    plt.title('Pass Prediction Probabilities')
+    plt.xlabel('Probability')
+    plt.ylabel('Frequency')
+    plt.legend(loc='upper right')
+    plt.savefig(os.path.join(output_dir, 'pass_histogram_epoch_{}.png'.format(epoch+1)))
+    plt.close()
+
+    # After the validation loop, plot the fail histogram
+    plt.figure(figsize=(12, 6))
+    plt.hist(fail_probs, bins=20, alpha=0.5, color='#ec6602',)
+    plt.title('Fail Prediction Probabilities')
+    plt.xlabel('Probability')
+    plt.ylabel('Frequency')
+    plt.legend(loc='upper right')
+    plt.savefig(os.path.join(output_dir, 'fail_histogram_epoch_{}.png'.format(epoch+1)))
+    plt.close()
+
     # Deep copy the model
     if val_acc > best_acc:
         best_acc = val_acc
@@ -201,7 +269,7 @@ model.load_state_dict(best_model_wts)
 nb_classes = 2
 confusion_matrix_best = torch.zeros(nb_classes, nb_classes)
 with torch.no_grad():
-    for inputs, furnace_nums, labels in val_loader:
+    for inputs, furnace_nums, labels, crystal_ids in val_loader:
         inputs, furnace_nums, labels = inputs.to(device), furnace_nums.float().to(device), labels.to(device)
         outputs = model(inputs, furnace_nums)
         _, preds = torch.max(outputs, 1)
@@ -214,14 +282,20 @@ torch.save(model.state_dict(), os.path.join(output_dir, 'model_best.pth'))
 # Confusion matrix
 confusion_matrix = torch.zeros(nb_classes, nb_classes)
 with torch.no_grad():
-    for inputs, furnace_nums, labels in val_loader:
+    for inputs, furnace_nums, labels, crystal_ids in val_loader:
         inputs, furnace_nums, labels = inputs.to(device), furnace_nums.float().to(device), labels.to(device)
         outputs = model(inputs, furnace_nums)
         _, preds = torch.max(outputs, 1)
         for t, p in zip(labels.view(-1), preds.view(-1)):
             confusion_matrix[t.long(), p.long()] += 1
+        
 
 print(confusion_matrix)
+
+# Delete all non-best model log files
+#for file in os.listdir(output_dir):
+#    if file.endswith('_log.txt'):
+#        os.remove(os.path.join(output_dir, file))
 
 # Plot and save the confusion matrix
 plt.figure(figsize=(10,10))
@@ -258,7 +332,6 @@ plt.ylabel('Actual label')
 plt.xlabel('Predicted label')
 plt.title(f'Confusion Matrix for Best Epoch ({best_epoch})', size = 15)
 plt.savefig(os.path.join(output_dir, f'confusion_matrix_best_{best_epoch}.png'))
-
 
 # calculate TPR, FPR, and AUC-ROC
 fpr, tpr, _ = roc_curve(val_labels, val_scores[:, 1], pos_label=1)  # 1 is the positive class
